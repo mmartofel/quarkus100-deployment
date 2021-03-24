d998dce48e9b7f5ef5328ccd0642ec3d6ce584fd
mmartofel <mmartofe@redhat.com>
2021-03-24 12:58:43 +0100

    change image at dev to short name

--- sec.sh
+++ sec.sh
@@ -1,2 +1,3 @@
 
 oc adm policy add-cluster-role-to-user cluster-admin -z argocd-cluster-argocd-application-controller -n openshift-gitops
+oc adm policy add-cluster-role-to-user cluster-admin -z argocd-cluster-argocd-server -n openshift-gitops
